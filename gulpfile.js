4390a35b72c235e257e9f3417cb85659b3cbc4b7
romanocode <carlos1996romano@gmail.com>
2026-05-17 13:59:38 -0500

    Compile Sass with Gulp and enable watch mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
-export function hola(done){
-    console.log("Hola Mundo desde gulp");
+import {src, dest, watch} from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+
+const sass = gulpSass(dartSass); 
+
+export function css(done) {
+    src('src/img/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('build/css') )
+
+    done()
+}
+
+export function dev(done) {
+    watch('src/img/scss/app.scss', css);
     done();
 }
