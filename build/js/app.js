dc07888cfd50c2fc589d08e46ee9446629297375
romanocode <carlos1996romano@gmail.com>
2026-05-22 20:30:08 -0500

    Gallery section with JS

---
+++ build/js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+    crearGaleria()
+})
+
+function crearGaleria(){
+    const CANTIDAD_IMAGENES = 16;
+    const galeria = document.querySelector('.galeria-imagenes')
+    
+
+    for(let i = 1; i <= CANTIDAD_IMAGENES; i++){
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        console.log(imagen)
+
+        galeria.appendChild(imagen)
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
-import {src, dest, watch} from 'gulp';
+import {src, dest, watch, series} from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
 const sass = gulpSass(dartSass); 
 
+export function js() {
+    return src('src/img/js/app.js')
+        .pipe(dest('build/js'))
+}
+
 export function css(done) {
     src('src/img/scss/app.scss',{sourcemaps:true})
         .pipe( sass().on('error', sass.logError) )
-        .pipe( dest('build/css',{sourcemaps:true}) )
+        .pipe( dest('build/css',{sourcemaps: '.'}) )
 
     done()
 }
 
 export function dev(done) {
     watch('src/img/scss/**/*.scss', css);
+    watch('src/img/js/**/*.js', js);
     done();
 }
+
+export default series(js, css, dev)
